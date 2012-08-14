e924db6e89a4c7d0aba4b79670965690a18f31d4
Tony Lambert <tonylambert@gmail.com>
2012-08-14 17:04:30 -0400

    Added a new NoteType enumeration and started on the logic of the game model through unit tests.

--- MusicNotes/MusicNotes.UnitTests/UnitTest1.cs
+++ MusicNotes/MusicNotes.UnitTests/UnitTest1.cs
@@ -6,13 +6,52 @@ using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
 namespace MusicNotes.UnitTests
 {
-    [TestClass]
-    public class GameTests
-    {
-        [TestMethod]
-        public void SimpleTest()
-        {
-           Assert.IsTrue( true );
-        }
-    }
+   [TestClass]
+   public class GameTests
+   {
+      [TestMethod]
+      public void ScorePointsForPressingButtonAtTheRightTime()
+      {
+         int currentNoteLocation = 10;
+         var userEnteredNote = NoteType.C;
+
+         int scorerXPosition = 10;
+         var noteType = NoteType.C;
+
+         int pointsScored = ScoreUserInput( userEnteredNote, currentNoteLocation, scorerXPosition, noteType );
+
+         Assert.AreEqual( 1, pointsScored );
+      }
+
+      private static int ScoreUserInput( NoteType userEnteredNote, int currentNoteLocation, int scorerXPosition, NoteType noteType )
+      {
+
+         int pointsScored = 0;
+
+         if ( userEnteredNote == noteType && currentNoteLocation == scorerXPosition )
+         {
+            pointsScored = 1;
+         }
+         else
+         {
+            pointsScored = -1;
+         }
+
+         return pointsScored;
+      }
+
+      [TestMethod]
+      public void LosePointsForPressingButtonAtTheWrongTime()
+      {
+         int currentNoteLocation = 10;
+         int scorerXPosition = 12;
+         var noteType = NoteType.C;
+
+         var userEnteredNote = NoteType.C;
+
+         int pointsScored = ScoreUserInput( userEnteredNote, currentNoteLocation, scorerXPosition, noteType );
+
+         Assert.AreEqual( -1, pointsScored );
+      }
+   }
 }
