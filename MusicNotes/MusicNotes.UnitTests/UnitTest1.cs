c789f6ea50c6fdb98f229773baead6d20e6e545f
Tony Lambert <tonylambert@gmail.com>
2012-08-13 23:58:10 -0400

    Created initial unit test project for MusicNotes. Only a throwaway test with an assert true in it so far.

---
+++ MusicNotes/MusicNotes.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MusicNotes.UnitTests
+{
+    [TestClass]
+    public class GameTests
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+           Assert.IsTrue( true );
+        }
+    }
+}
