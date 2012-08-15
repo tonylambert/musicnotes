04840e8a0592b6f8d9964f72578d22b2cc603dbc
Tony Lambert <tonylambert@gmail.com>
2012-08-14 22:57:59 -0400

    Moved the Game and GameConfig classes out into the MusicNotes project instead of the unit test project since its production code.

---
+++ MusicNotes/MusicNotes/Game.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicNotes
+{
+   public class Game
+   {
+      protected int _currentNoteLocation = 0;
+      protected int _totalScore = 0;
+      protected GameConfig _gameConfig;
+
+      public Game( GameConfig gameConfig )
+      {
+         _gameConfig = gameConfig;
+      }
+
+      public int TotalScore
+      {
+         get
+         {
+            return _totalScore;
+         }
+      }
+
+      private int GetSpeedOfNote()
+      {
+         return _gameConfig.SpeedOfNote;
+      }
+
+      public int MoveNote( TimeSpan elapsedTime )
+      {
+         _currentNoteLocation += (int) (elapsedTime.TotalSeconds * GetSpeedOfNote());
+         return _currentNoteLocation;
+      }
+
+      public ScoreResult ScoreUserInput( NoteType userEnteredNote, NoteType noteType )
+      {
+         int pointsScored = _gameConfig.PointsGainedWhenCorrect;
+         ScoreResult result = ScoreResult.Correct;
+
+         if ( userEnteredNote != noteType && _currentNoteLocation != _gameConfig.PositionOfTargetArea )
+         {
+            pointsScored = _gameConfig.PointsLostWhenWrongNoteAndTime;
+            result = ScoreResult.WrongTimeAndNote;
+         }
+         else if ( userEnteredNote != noteType )
+         {
+            pointsScored = _gameConfig.PointsLostWhenWrongNote;
+            result = ScoreResult.WrongNote;
+         }
+         else if ( _currentNoteLocation != _gameConfig.PositionOfTargetArea )
+         {
+            pointsScored = _gameConfig.PointsLostWhenWrongTime;
+            result = ScoreResult.WrongTime;
+         }
+
+         _totalScore += pointsScored;
+         return result;
+      }
+   }
+}
---
+++ MusicNotes/MusicNotes/GameConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicNotes
+{
+   public struct GameConfig
+   {
+      public int SpeedOfNote;
+      public int PointsLostWhenWrongNote;
+      public int PointsLostWhenWrongTime;
+      public int PointsLostWhenWrongNoteAndTime;
+      public int PointsGainedWhenCorrect;
+      public int PositionOfTargetArea;
+
+      public static GameConfig DefaultGameConfig
+      {
+         get
+         {
+            return new GameConfig
+            {
+               SpeedOfNote = 3,
+               PointsGainedWhenCorrect = 3,
+               PointsLostWhenWrongNote = -2,
+               PointsLostWhenWrongNoteAndTime = -3,
+               PointsLostWhenWrongTime = -1,
+               PositionOfTargetArea = 10,
+            };
+         }
+      }
+   }
+}
